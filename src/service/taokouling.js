456a8edb26c70be97c4b0b04dcf769f0c368fab3
chengeng <chengeng@bloggeng.com>
2021-03-22 15:35:41 +0800

    修改解析口令的接口

--- src/service/taokouling.js
+++ src/service/taokouling.js
@@ -3,64 +3,57 @@
  * API from taokouling
  */
 
-const request = require('request');
+const request = require('request')
 
 class TKLService {
-    constructor({apiKey, siteId, adzoneId, uid}) {
-        this.apiKey  = apiKey
-        this.siteId  = siteId
-        this.adzoneId  = adzoneId
-        this.uid  = uid
-    }
-    /**
-     * 淘口令解析
-     */
-    check(text) {
-        return new Promise((resolve, reject) => {
-            request.get(
-                `http://api.taokouling.com/tkl/tkljm?apikey=${this.apiKey}&tkl=${encodeURI(text)}`,
-                {json: true},
-                (_err, _res, body) => {
-                    if (body && body.code === 1) {
-                        const data = body.url.split('?');
-                        const obj = data[1].split('&');
-                        let itemId = '';
-                        for (let i = 0; i < obj.length; i++) {
-                            const [key, value] = obj[i].split('=');
-                            if (key === 'id') {
-                                itemId = value;
-                                break;
-                            }
-                        }
-                        if (itemId) {
-                            resolve(itemId);
-                        }
-                    }
-                    reject('Check pwd error');
-                }
-            );
-        });
-    }
+  constructor({ apiKey, siteId, adzoneId, uid }) {
+    this.apiKey = apiKey
+    this.siteId = siteId
+    this.adzoneId = adzoneId
+    this.uid = uid
+  }
+  /**
+   * 淘口令解析
+   */
+  check(text) {
+    return new Promise((resolve, reject) => {
+      // const url =  `http://api.taokouling.com/tkl/tkljm?apikey=${this.apiKey}&tkl=${encodeURI(text)}`
+      const url = `https://taodaxiang.com/taopass/parse/get?&content=${encodeURI(text)}`
+      request.get(url, { json: true }, (_err, _res, body) => {
+        if (body && body.code === 0) {
+          const data = body.data.url.split('?')
+          const obj = data[1].split('&')
+          let itemId = ''
+          for (let i = 0; i < obj.length; i++) {
+            const [key, value] = obj[i].split('=')
+            if (key === 'id') {
+              itemId = value
+              break
+            }
+          }
+          if (itemId) {
+            resolve(itemId)
+          }
+        }
+        reject('Check pwd error')
+      })
+    })
+  }
 
-    /**
-     * 高佣转链接
-     */
-    createLink(itemId) {
-        return new Promise((resolve, reject) => {
-            request.get(
-                `https://api.taokouling.com/tkl/TbkPrivilegeGet?apikey=${this.apiKey}&itemid=${itemId}&siteid=${this.siteId}&adzoneid=${this.adzoneId}&uid=${this.uid}`,
-                {json: true},
-                (_err, _res, body) => {
-                    if (body && body.result && body.result.data) {
-                        resolve(body.result.data);
-                    } else {
-                        reject('Create link error');
-                    }
-                }
-            );
-        });
-    }
+  /**
+   * 高佣转链接
+   */
+  createLink(itemId) {
+    return new Promise((resolve, reject) => {
+      request.get(`https://api.taokouling.com/tkl/TbkPrivilegeGet?apikey=${this.apiKey}&itemid=${itemId}&siteid=${this.siteId}&adzoneid=${this.adzoneId}&uid=${this.uid}`, { json: true }, (_err, _res, body) => {
+        if (body && body.result && body.result.data) {
+          resolve(body.result.data)
+        } else {
+          reject('Create link error')
+        }
+      })
+    })
+  }
 }
 
 module.exports = TKLService
-
