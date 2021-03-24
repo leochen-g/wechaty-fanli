9ac46222c6e2039eb94a5d105f2fa61aaf102f6c
chengeng <chengeng@bloggeng.com>
2021-03-24 17:18:58 +0800

    修改地址

--- src/service/taokouling.js
+++ src/service/taokouling.js
@@ -4,7 +4,8 @@
  */
 
 const request = require('request')
-
+const axios = request('axios')
+const FormData = require('form-data')
 class TKLService {
   constructor({ apiKey, siteId, adzoneId, uid }) {
     this.apiKey = apiKey
@@ -18,25 +19,60 @@ class TKLService {
   check(text) {
     return new Promise((resolve, reject) => {
       // const url =  `http://api.taokouling.com/tkl/tkljm?apikey=${this.apiKey}&tkl=${encodeURI(text)}`
-      const url = `https://taodaxiang.com/taopass/parse/get?&content=${encodeURI(text)}`
-      request.get(url, { json: true }, (_err, _res, body) => {
-        if (body && body.code === 0) {
-          const data = body.data.url.split('?')
-          const obj = data[1].split('&')
-          let itemId = ''
-          for (let i = 0; i < obj.length; i++) {
-            const [key, value] = obj[i].split('=')
-            if (key === 'id') {
-              itemId = value
-              break
+      // const url = `https://taodaxiang.com/taopass/parse/get?&content=${encodeURI(text)}`
+      var data = new FormData()
+      data.append('kouling', text)
+      data.append('isitem', ' false')
+      data.append('isquan', ' false')
+      const config = {
+        method: 'post',
+        url: 'https://www.2017taoke.com/tool/kouling.html?submit',
+        headers: {
+          referer: 'https://www.2017taoke.com/tool/kouling.html',
+          ...data.getHeaders(),
+        },
+        data: data,
+      }
+      axios(config)
+        .then(function (response) {
+          if (response.success) {
+            const data = response.url.split('?')
+            const obj = data[1].split('&')
+            let itemId = ''
+            for (let i = 0; i < obj.length; i++) {
+              const [key, value] = obj[i].split('=')
+              if (key === 'id') {
+                itemId = value
+                break
+              }
+            }
+            if (itemId) {
+              resolve(itemId)
             }
           }
-          if (itemId) {
-            resolve(itemId)
-          }
-        }
-        reject('Check pwd error')
-      })
+          reject('Check pwd error')
+        })
+        .catch(function (error) {
+          reject('Check pwd error')
+        })
+      // request.get(url, { json: true }, (_err, _res, body) => {
+      //   if (body && body.code === 0) {
+      //     const data = body.data.url.split('?')
+      //     const obj = data[1].split('&')
+      //     let itemId = ''
+      //     for (let i = 0; i < obj.length; i++) {
+      //       const [key, value] = obj[i].split('=')
+      //       if (key === 'id') {
+      //         itemId = value
+      //         break
+      //       }
+      //     }
+      //     if (itemId) {
+      //       resolve(itemId)
+      //     }
+      //   }
+      //   reject('Check pwd error')
+      // })
     })
   }
 
